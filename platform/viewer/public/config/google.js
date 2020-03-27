c61ddcaf1a287f8f343c22fce187f8401bd56ff3
Alexis Rengifo <alexisrengifo@gmail.com>
2020-03-26 19:06:45 -0500

    Using alexisrengifo@gmail.com id

--- platform/viewer/public/config/google.js
+++ platform/viewer/public/config/google.js
@@ -13,13 +13,14 @@ window.config = {
       // ~ REQUIRED
       // Authorization Server URL
       authority: 'https://accounts.google.com',
-      client_id: 'YOURCLIENTID.apps.googleusercontent.com',
+      client_id:
+        '409596160399-jvos1lrt8q6lqc1u2u9fi2vokn4upe4a.apps.googleusercontent.com',
       redirect_uri: '/callback', // `OHIFStandaloneViewer.js`
       response_type: 'id_token token',
       scope:
         'email profile openid https://www.googleapis.com/auth/cloudplatformprojects.readonly https://www.googleapis.com/auth/cloud-healthcare', // email profile openid
       // ~ OPTIONAL
-      post_logout_redirect_uri: '/logout-redirect.html',
+      post_logout_redirect_uri: '/',
       revoke_uri: 'https://accounts.google.com/o/oauth2/revoke?token=',
       automaticSilentRenew: true,
       revokeAccessTokenOnSignout: true,
